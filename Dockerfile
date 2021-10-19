d62a42d155ca316ca7406b055367ce4021b78382
Tim Etchells <tetchel@gmail.com>
2021-10-19 13:33:05 -0700

    Fix workflow
    Signed-off-by: Tim Etchells <tetchel@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM quay.io/redhat-certification/chart-verifier:1.2.3
 RUN ln -s /app/chart-verifier /usr/local/bin/chart-verifier
 
 RUN rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm \
-    && microdnf -y install jq \
+    && microdnf -y install jq bc \
     && microdnf clean all \
     && rm -rf /var/cache/dnf
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "$KUBECONFIG" ]; then
 	exit 1
 fi
 
-chart_uri="$1"
+chart_uri=$1
 echo "Chart URI to certify is '$chart_uri'"
 
 if [ -z "$chart_uri" ]; then
@@ -14,14 +14,32 @@ if [ -z "$chart_uri" ]; then
 	exit 1
 fi
 
+report_type=$2
+echo "Report type is '$report_type'"
 
-shift
-extra_args="$@"
+shift; shift
 
-set -ex
+set -e
 
+echo "::group::Printing usage"
 chart-verifier verify --help
+echo "::endgroup::"
 
-chart-verifier verify --kubeconfig $KUBECONFIG $extra_args $chart_uri 2>&1 | tee chart-verifier-report.yaml
+# echo "::group::Running 'verify'"
+set -x
+chart-verifier verify --kubeconfig $KUBECONFIG $* $chart_uri 2>&1 | tee chart-verifier-report.yaml
+# echo "::endgroup::"
+# echo "::group::Running 'report'"
+chart-verifier report $report_type chart-verifier-report.yaml | tee results.json
+set +x
+# echo "::endgroup::"
 
-chart-verifier report chart-verifier-report.yaml | tee final-report.json | jq
+passed=$(cat results.json | jq -r '.results.passed')
+failed=$(cat results.json | jq -r '.results.failed')
+
+echo "\u001b[32m$passed\u001b[0m checks passed"
+echo "\u001b[31m$failed\u001b[0m checks failed"
+
+at_least_one_failed=$(echo $failed ">" "0" | bc -l)
+
+exit $at_least_one_failed
